d4f7ffee15f1c0eadcee9fcfd3df38c5a0e67984
NicJr1110 <nicholasjnorton@gmail.com>
2019-12-22 16:43:30 +0000

    Create Redux Store and link to devtools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
+import reducer from "./Data/reducer";
+import initial from "./Data/initial";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +13,17 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+const store = createStore(
+    reducer,
+    initial,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => {
+    let state = store.getState()
+    console.log(state.totalTeams)
+}) 
+
+store.dispatch({type: "increment"})

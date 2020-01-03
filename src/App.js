1c00433e7a7989d4784d77ab04ee7f21cd302292
NicJr1110 <nicholasjnorton@gmail.com>
2020-01-03 14:15:30 +0000

    remove unused component imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import TeamCounter from "./Components/Counter/TeamCounter";
-//import PlayerCounter from "./Components/Counter/PlayerCounter";
 import Landing from "./Components/Landing";
-import InputPlayerGroup from "./Components/InputPlayerGroup";
-//import PlayerList from "./Components/PlayerList";
-import TeamSplitter from "./Components/TeamSplitter";
-import TeamCards from "./Components/TeamCards";
 import Application from "./Components/Application";
 
 function App() {
